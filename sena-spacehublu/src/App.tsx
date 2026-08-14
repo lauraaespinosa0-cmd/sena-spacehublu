d966b765f23df0f2bbe3669476a139ee24a63316
lauraaespinosa0-cmd <lauraaespinosa0@gmail.com>
2026-08-14 17:03:11 -0500

    sena-spacehublu

---
+++ sena-spacehublu/src/App.tsx
@@ -0,0 +1,12 @@
+import InicioSesion from "./components/InicioSesion/InicioSesion";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <InicioSesion />
+    </>
+  );
+}
+
+export default App;
